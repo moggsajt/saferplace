7dfe45d89f641d16a76a4d97422131c6037f200c
Joel Sewing <joel.sewing@gmail.com>
2018-04-14 14:01:35 +0200

    Aid and relief added

---
+++ src/app/sections/aid/aid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'safe-aid',
+  templateUrl: './aid.component.html',
+  styleUrls: ['./aid.component.css']
+})
+export class AidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
